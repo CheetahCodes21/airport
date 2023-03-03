9442cfb47716bd073eb7e15e2bc836b54167b2d1
Cheetah2107 <mcpai217@gmail.com>
2023-03-03 19:12:30 +0530

    Step4: Form validations and flagging error

--- src/pages/SearchForm.js
+++ src/pages/SearchForm.js
@@ -1,5 +1,12 @@
 import React,{useState} from 'react'
 const SearchForm = () =>{
+
+    const [Errors,setErrors]=useState({
+        DepartureName:false,
+        Checkin:false,
+        Checkout:false
+    });
+
     const[DepartureName,setDepartureName]=useState('Delhi');
     const[Checkin,setCheckin]=useState('');
     const[Checkout,setCheckout]=useState('');
@@ -7,16 +14,26 @@ const SearchForm = () =>{
     const DepartureHandler = (e) =>{
       const {value} = e.target;
       setDepartureName(value);
+      if(e.target.value){
+        setErrors((err)=>({...err, DepartureName:null}))
+    }
+
     }
 
     const CheckinHandler = (e)=>{
       const {value}=e.target;
       setCheckin(value);
+      if(e.target.value){
+        setErrors((err)=>({...err, Checkin:null}))
+    }
     }
 
     const CheckoutHandler = (e) =>{
         const {value} = e.target;
         setCheckout(value);
+        if(e.target.value){
+            setErrors((err)=>({...err, Checkout:null}))
+        }
     }
     
     const SubmitHandler = (e) =>{
@@ -24,6 +41,16 @@ const SearchForm = () =>{
         console.log(DepartureName);
         console.log(Checkin);
         console.log(Checkout);
+
+        if(DepartureName && Checkin && Checkout){
+            alert('Form has been Submitted');
+        }else{
+            setErrors({
+                DepartureName:!DepartureName,
+                Checkin:!Checkin,
+                Checkout:!Checkout
+            })
+        }
     }
     return(
     <form action="/results.html" method="post">
@@ -31,6 +58,7 @@ const SearchForm = () =>{
             <div className="heading mb-1">Departure Airport</div>
             <div className="placeholder placeholder-airport">
                 <input type="text" placeholder="Departure Airport" className="placeholder placeholder-airport" onChange={DepartureHandler} value={DepartureName} />
+                {(Errors && Errors.DepartureName)?<h3 style={{backgroundColor: 'rgba(100, 100, 100, 0.5)'}}>Enter DepartureName</h3>:null}
             </div> <i
                 className="fas fa-map-marker-alt input-icon"></i>
         </label>
@@ -39,10 +67,12 @@ const SearchForm = () =>{
                 <div className="heading mb-1">Parking Check-In</div>
                 <div className="placeholder">
                     <input name="checkin" type="date" placeholder="Parking Check-Out" className="placeholder placeholder-airport" style={{width:'100%'}} onChange={CheckinHandler}/>
+                    {(Errors && Errors.Checkin)?<h3 style={{backgroundColor: 'rgba(100, 100, 100, 0.5)'}}>Enter checkin date</h3>:null}
                 </div> 
             </label> <label className="col-sm-6 p-0 pl-sm-0 date_input">
                 <div className="heading mb-1">Parking Check-Out</div>
                     <input name="Check-Out" type="date" placeholder="Parking Check-Out" className="placeholder placeholder-airport" style={{width:'100%'}} onChange={CheckoutHandler}/>
+                    {(Errors && Errors.Checkout)?<h3 style={{backgroundColor: 'rgba(100, 100, 100, 0.5)'}}>Enter checkout date</h3>:null}
                
             </label></div>
         <div className="col-12 col-xl-2 p-0 pl-xl-3 my-3 my-xl-0">
@@ -53,5 +83,4 @@ const SearchForm = () =>{
 </form>
     );
 }
-
-export default SearchForm
+export default SearchForm;
